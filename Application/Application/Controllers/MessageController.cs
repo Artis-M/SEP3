5e20e514f3030d5f0d93ef88ae96fa7b873efaab
jamborpal <58312465+jamborpal@users.noreply.github.com>
2020-11-30 12:26:26 +0100

    HttpPart 1.0
    needs connection through ModelManager with the Socket connection added

---
+++ Application/Application/Controllers/MessageController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Tier2.Model;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private IMessageService MessageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            this.MessageService = messageService;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<ActionResult<IList<Chatroom>>> getChatRooms()
+        {
+            try
+            {
+                IList<Chatroom> chatrooms = await MessageService.getChatrooms();
+                return Ok(chatrooms);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("groupId")]
+        public async Task<ActionResult<Chatroom>> getMessagesByRoomId([FromBody] int Id)
+        {
+            Chatroom chatroom = new Chatroom();
+            IList<Chatroom> chatrooms = await MessageService.getChatrooms();
+            try
+            {
+                foreach (var Chatroom in chatrooms)
+                {
+                    if (Chatroom.ID == Id)
+                    {
+                        chatroom = Chatroom;
+                    }
+                }
+
+                return Ok(chatroom);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Message>> sendMessage([FromBody] Message message, [FromBody] int chatRoomId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Message sentMessage =  await MessageService.sendMessage(message, chatRoomId);
+                return Created($"/{sentMessage.Id}", sentMessage);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
