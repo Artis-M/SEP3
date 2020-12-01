85d5880dfb00f1c3c4733544df789eddc7cd3647
Artis <mundurs.artis@gmail.com>
2020-12-01 22:12:19 +0100

    I did a thngy
    Added a field in the blazor page that sends a message fragment on each new keypress, which might be overkill, but it actually worked so I am proud of it.

--- Client/ChatClient/ChatClient/Models/Message.cs
+++ Client/ChatClient/ChatClient/Models/Message.cs
@@ -1,10 +1,11 @@
-﻿namespace Models
+﻿using MongoDB.Bson;
+
+namespace Models
 {
     public class Message
     {
         public string message { get; set; }
         public int Id { get; set; }
-        
-        public int authorID { get; set; }
+        public ObjectId authorID { get; set; }
     }
 }
---
+++ Client/ChatClient/ChatClient/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace ChatClient.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public ObjectId userId { get; set; }
+    }
+}
