beefd9efd63f51494c781d4f0d05d92a7cdd8a75
jamborpal <58312465+jamborpal@users.noreply.github.com>
2020-12-03 10:16:37 +0100

    Model methods

--- Application/Application/Model/Model.cs
+++ Application/Application/Model/Model.cs
@@ -1,12 +1,20 @@
 ﻿﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using MongoDB.Bson;
+ using Application.Model;
+ using MongoDB.Bson;
 
 namespace Tier2.Model
 {
     public interface Model
     {
-        public Task<IList<Chatroom>> getChatroom(string json);
-        public Task sendMessage(Message message, ObjectId chatroomId);
+        public Task RequestChatrooms();
+        public Task RequestUsers();
+        public Task RequestTopics();
+        public Task SendMessage(Message message, string chatroomId);
+        public Task Register(Account account);
+        public Task<List<Chatroom>> RecieveChatrooms();
+        public Task<List<User>> RecieveUsers();
+        public Task<List<Topic>> RecieveTopics();
+
     }
 }
