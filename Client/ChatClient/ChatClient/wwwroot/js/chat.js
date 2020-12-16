097c38517c0eaa920aa7040ae03bb60e4b10b52e
Artis <mundurs.artis@gmail.com>
2020-12-16 11:51:05 +0100

    Removed unused code from the JS

--- Client/ChatClient/ChatClient/wwwroot/js/chat.js
+++ Client/ChatClient/ChatClient/wwwroot/js/chat.js
@@ -1,6 +1,5 @@
 ﻿function scroll(elementID) {
     let elem = document.getElementById(elementID);
-    // elem.scrollTop = elem.scrollHeight;
     elem.scrollBy({
         top: elem.scrollHeight,
         left: 0,
@@ -9,7 +8,6 @@
 }
 function scrollMessageFragment(className) {
     let elem = document.getElementsByClassName(className);
-    // elem.scrollTop = elem.scrollHeight;
     for(let i = 0; i < elem.length; i++){
         console.log("??????");
         console.log(i.toString());
